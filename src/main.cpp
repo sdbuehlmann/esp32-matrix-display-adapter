28c3e33703902bf7681aad4c5998daea68f68b2e
Simon Bühlmann <sdbuehlmann@gmail.com>
2026-05-21 12:14:37 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  int result = myFunction(2, 3);
+
+  Serial.print("Result: ");
+  Serial.println(result);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("Hello world!");
+  delay(1000);
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
